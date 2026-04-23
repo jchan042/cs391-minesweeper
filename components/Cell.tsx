6c3446705e32b39c276f6160432870c12a4f01f1
Kimaya Jadhav <kimayaj@bu.edu>
2026-04-23 15:32:52 -0400

    Handles cells on the board

---
+++ components/Cell.tsx
@@ -0,0 +1,72 @@
+"use client";
+import styled from "styled-components";
+
+const StyledCell = styled.div<{ $revealed: boolean }>`
+  width: 40px;
+  height: 40px;
+  border: 1px solid #444;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-weight: bold;
+  cursor: pointer;
+
+  background-color: ${(props) =>
+    props.$revealed ? "#ddd" : "#999"};
+
+  &:hover {
+    background-color: ${(props) =>
+    props.$revealed ? "#ddd" : "#bbb"};
+  }
+`;
+
+type CellProps = {
+    isBomb: boolean;
+    isRevealed: boolean;
+    isFlagged: boolean;
+    adjacent: number;
+    onReveal: () => void;
+    onFlag: () => void;
+};
+
+export default function Cell({
+                                 isBomb,
+                                 isRevealed,
+                                 isFlagged,
+                                 adjacent,
+                                 onReveal,
+                                 onFlag,
+                             }: CellProps) {
+
+    // handles users flagging by right clicking
+    const handleRightClick = (e: React.MouseEvent) => {
+        e.preventDefault(); // prevents browser context menu
+        if (!isRevealed) {
+            onFlag();
+        }
+    };
+
+    // determines what is displayed in each cell
+    const renderContent = () => {
+        if (!isRevealed) {
+            return isFlagged ? "🚩" : "";
+        }
+
+        if (isBomb) return "💣";
+        if (adjacent > 0) return adjacent;
+
+        return "";
+    };
+
+    return (
+        <StyledCell
+            onClick={onReveal}
+            onContextMenu={handleRightClick}
+            $revealed={isRevealed}
+        >
+            {renderContent()}
+        </StyledCell>
+    );
+}
