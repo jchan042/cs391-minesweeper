9ae81ce830f1f78563a56915700e3218c266a051
Gina Carpneter <gcarp@bu.edu>
2026-04-28 19:51:42 -0400

    added name to files in comments

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+// gina
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
--- components/Logout.tsx
+++ components/Logout.tsx
@@ -1,3 +1,4 @@
+// gina
 "use client";
 
 import styled from "styled-components";
